8f6788cbc84b068dbc0a1eeef8b647255cb48b2e
Anamika Gupta <65801101+anamikarec@users.noreply.github.com>
2021-12-24 16:03:39 +0530

    all-the-pages-are-added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,50 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./navbar.module.css";
 import UserForm from "../UserForm/UserForm";
+import axios from "axios";
 
 const NavBar = () => {
   return (
-    <div
-      style={{
-        backgroundImage:
-          "url('https://media.istockphoto.com/photos/restaurant-gourmet-food-on-dark-background-picture-id902558446?k=20&m=902558446&s=170667a&w=0&h=-mr8LaG_pKDXhwA5n-jkYeqMsKwUtNdSFIIggfjicu0=')",
-        height: "500px",
-        width: "100%",
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat"
-      }}
-    >
-      <div className={styles.navbar}>
-        <Link to="/" className={styles.navbarLink}>
-          Home
-        </Link>
-        <Link to="/login" className={styles.navbarLink}>
-          Login Page
-        </Link>
-        <Link to="/signup" className={styles.navbarLink}>
-          Sign Up
-        </Link>
-        <Link to="/addrestaurants" className={styles.navbarLink}>
-          Add Reastaurants
-        </Link>
-      </div>
-      <div className={styles.imgLogo}>
-        <img
-          src="https://b.zmtcdn.com/web_assets/8313a97515fcb0447d2d77c276532a511583262271.png"
-          alt="image"
-          height="50"
-        />
-      </div>
-      <div className={styles.imgLogo}>
-        <p style={{ fontSize: "40px" }}>
-          Discover the best food & drinks in Mughalsarai
-        </p>
+    <>
+      <div
+        style={{
+          backgroundImage:
+            "url('https://media.istockphoto.com/photos/restaurant-gourmet-food-on-dark-background-picture-id902558446?k=20&m=902558446&s=170667a&w=0&h=-mr8LaG_pKDXhwA5n-jkYeqMsKwUtNdSFIIggfjicu0=')",
+          height: "500px",
+          width: "100%",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat"
+        }}
+      >
+        <div className={styles.navbar}>
+          <Link to="/" className={styles.navbarLink}>
+            Home
+          </Link>
+          <Link to="/login" className={styles.navbarLink}>
+            Login Page
+          </Link>
+          <Link to="/signup" className={styles.navbarLink}>
+            Sign Up
+          </Link>
+          <Link to="/addrestaurants" className={styles.navbarLink}>
+            Add Reastaurants
+          </Link>
+        </div>
+        <div className={styles.imgLogo}>
+          <img
+            src="https://b.zmtcdn.com/web_assets/8313a97515fcb0447d2d77c276532a511583262271.png"
+            alt="image"
+            height="50"
+          />
+        </div>
+        <div className={styles.imgLogo}>
+          <p style={{ fontSize: "40px" }}>
+            Discover the best food & drinks in Mughalsarai
+          </p>
+        </div>
       </div>
       <div>
         <UserForm />
       </div>
-    </div>
+    </>
   );
 };
 export default NavBar;
--- src/Components/UserForm/UserForm.jsx
+++ src/Components/UserForm/UserForm.jsx
@@ -1,4 +1,28 @@
+import React, { useState } from "react";
+import axios from "axios";
+
 const UserForm = () => {
+  const [userInput, setUser] = useState("Arrabiata");
+  const [data, setData] = useState([]);
+  const handleSearch = (userInput) => {
+    const config = {
+      method: "get",
+      url: `https://www.themealdb.com/api/json/v1/1/search.php?s=${userInput}`
+    };
+    return axios(config);
+  };
+
+  const handleChange = (e) => {
+    setUser(e.target.value);
+  };
+  const handleSubmit = () => {
+    console.log(userInput);
+    handleSearch(userInput).then(
+      (res) => setData(res.data.meals)
+      // var foodData = res.data.meals;
+      // console.log(foodData);
+    );
+  };
   return (
     <>
       <div style={{ display: "flex", justifyContent: "center" }}>
@@ -10,7 +34,7 @@ const UserForm = () => {
             fontSize: "18px",
             borderTopLeftRadius: "10px",
             borderBottomLeftRadius: "10px",
-            border: "0",
+            border: "1px solid black",
             borderRight: "1px solid grey",
             marginTop: "50px"
           }}
@@ -23,17 +47,71 @@ const UserForm = () => {
         </select>
         <input
           type="text"
+          onChange={handleChange}
+          style={{
+            padding: "10px",
+            fontSize: "18px",
+            width: "400px",
+            // borderTopRightRadius: "10px",
+            // borderBottomRightRadius: "10px",
+            // border: "0",
+            marginTop: "50px",
+            border: "1px solid black"
+          }}
+          placeholder="🔍Search for restaurant, cuisine or a dish"
+        />
+        <input
+          type="submit"
+          onClick={handleSubmit}
           style={{
             padding: "10px",
             fontSize: "18px",
             width: "400px",
             borderTopRightRadius: "10px",
             borderBottomRightRadius: "10px",
-            border: "0",
-            marginTop: "50px"
+            // border: "0",
+            marginTop: "50px",
+            border: "1px solid black"
           }}
           placeholder="🔍Search for restaurant, cuisine or a dish"
         />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          gap: "2rem",
+          width: "90%",
+          margin: "auto"
+        }}
+      >
+        {data.map((item) => {
+          return (
+            // <h3>{item.idMeal}</h3>
+            <div
+              style={{
+                textAlign: "center",
+                borderRadius: "10px",
+                backgroundColor: "cornsilk"
+              }}
+            >
+              <img
+                src={item.strMealThumb}
+                alt="img"
+                height="250"
+                width="250"
+                style={{
+                  borderTopLeftRadius: "10px",
+                  borderTopRightRadius: "10px"
+                }}
+              />
+              <p>Order Id : {item.idMeal}</p>
+              <p>{item.strMeal}</p>
+              <p>Meal Time: {item.strCategory}</p>
+            </div>
+          );
+        })}
       </div>
     </>
   );
