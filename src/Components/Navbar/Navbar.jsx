937c97daeff3a6e31f76f190326b96248dd5cd08
Anamika Gupta <65801101+anamikarec@users.noreply.github.com>
2021-12-24 09:32:13 +0530

    Initial commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./navbar.module.css";
+
+const NavBar = () => {
+  return (
+    <div className={styles.navbar}>
+      <Link to="/" className={styles.navbarLink}>
+        Home
+      </Link>
+      <Link to="/login" className={styles.navbarLink}>
+        Login Page
+      </Link>
+      <Link to="/signup" className={styles.navbarLink}>
+        Sign Up
+      </Link>
+      <Link to="/addrestaurants" className={styles.navbarLink}>
+        Add Reastaurants
+      </Link>
+    </div>
+  );
+};
+export default NavBar;
